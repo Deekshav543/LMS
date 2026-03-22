3748267da67cf3be395bdd8c830c504710e42e78
Deeksha V <deekshav340@gmail.com>
2026-03-22 12:48:54 +0530

    Connect frontend to backend (Render)

--- server/server.js
+++ server/server.js
@@ -20,3 +20,25 @@ start().catch((err) => {
   process.exit(1)
 })
 
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const { connectDB } = require('./src/config/db');
+
+// // ✅ CORRECT PATH
+// const app = require('./src/app');
+
+// const PORT = process.env.PORT || 5000;
+
+// async function start() {
+//   await connectDB();
+
+//   app.listen(PORT, () => {
+//     console.log("[server] Listening on port"+PORT);
+//   });
+// }
+
+// start().catch((err) => {
+//   console.error('[server] Failed to start:', err);
+//   process.exit(1);
+// });
